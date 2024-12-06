d3fe532bf69facdc694b723f85091a5ad80f24ca
jumariaco <justinemariacourt@hotmail.fr>
2024-12-06 11:27:47 +0100

    feat(ui): add text component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Input from './Components/input'
+import Text from './Text';
 
 function App() {
   const [name, setName] = useState('')
   return (
     <>
+    <Text as="h1" className="title">
+        Bienvenue!
+      </Text>
+      <Text as="p" className="subtitle">
+        Quel est votre nom?
+      </Text>
       <Input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} className="input" id="input" />
     </>
   )
